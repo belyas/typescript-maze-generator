2859f150aaf0f12c426a1ec59015fd82552fed47
yassine belkaid <yassine.belkaid87@gmail.com>
2019-10-16 12:23:03 +0100

    changed Position interface to CellPosition as it causes conflict

--- app/cell.ts
+++ app/cell.ts
@@ -1,58 +1,58 @@
-interface Position {
-    readonly row: number;
-    readonly col: number;
+interface CellPosition {
+  row: number;
+  col: number;
 }
 
 class Cell {
-    /** @var string holds cell's value to be colored */
-    value: string;
-    /** @var number|null cell's row position */
-    row: number | null;
-    /** @var number|null cell's column position */
-    col: number | null;
+  /** @var string holds cell's value to be colored */
+  value: string;
+  /** @var number|null cell's row position */
+  row: number | null;
+  /** @var number|null cell's column position */
+  col: number | null;
 
-    constructor(type: string) {
-        this.value = type;
-        this.row = null;
-        this.col = null;
+  constructor(type: string) {
+    this.value = type;
+    this.row = null;
+    this.col = null;
+  }
+
+  /**
+   * Get cell specific color
+   *
+   * @return string
+   */
+  getColor(): string {
+    let color: string;
+
+    switch (this.value) {
+      case "w":
+        color = "#212121";
+        break;
+      case "e":
+        color = "#fff";
+        break;
+      case "s":
+        color = "#22cc5b";
+        break;
+      case "f":
+        color = "#e52727";
+        break;
+      default:
+        color = "gray";
+        break;
     }
 
-    /**
-     * Get cell specific color
-     * 
-     * @return string
-     */
-    getColor (): string {
-        let color: string;
+    return color;
+  }
 
-        switch (this.value) {
-            case 'w':
-                color = '#212121';
-                break;
-            case 'e':
-                color = '#fff';
-                break;
-            case 's':
-                color = '#22cc5b';
-                break;
-            case 'f':
-                color = '#e52727';
-                break;
-            default:
-                color = 'gray';
-                break;
-        }
-
-        return color;
-    }
-
-    /**
-     * Set cell's row|colun
-     * 
-     * @return void
-     */
-    setPosition (position: Position): void {
-        this.row = position.row;
-        this.col = position.col;
-    }
-};
+  /**
+   * Set cell's row|colun
+   *
+   * @return void
+   */
+  setPosition(position: CellPosition): void {
+    this.row = position.row;
+    this.col = position.col;
+  }
+}
