953b77ecaf525fa473d3831c4d9bada98c0d2320
yassine belkaid <yassine.belkaid87@gmail.com>
2019-10-19 18:31:27 +0100

    added clear path functionality

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 const regenerateMaze = document.getElementById("maze");
 const pathSolver = document.getElementById("path-solver");
+const pathClearer = document.getElementById("path-clearer");
 const canvas = document.getElementById("pp-maze");
 const ctx = canvas.getContext("2d");
 const MAZE_SIZE_LEN = 30;
@@ -50,7 +51,33 @@ const pathSolverFn = () => {
   requestAnimationFrame(drawPath);
 };
 
+const pathClearerFn = () => {
+  PpMaze._ctx.clearRect(0, 0, PpMaze._width, PpMaze._height);
+
+  for (let row = 0, rowsLen = PpMaze.data.length; row < rowsLen; row++) {
+    for (let col = 0, numCols = PpMaze.data[0].length; col < numCols; col++) {
+      let rectX = col * PpMaze._cellLength;
+      let rectY = row * PpMaze._cellLength;
+      let currentCell = PpMaze.data[row][col];
+
+      if (currentCell.value === PATH_WALL) {
+        currentCell.value = ROUTE_WALL;
+      }
+
+      PpMaze._ctx.fillStyle = currentCell.getColor();
+
+      PpMaze._ctx.fillRect(
+        rectX,
+        rectY,
+        PpMaze._cellLength,
+        PpMaze._cellLength
+      );
+    }
+  }
+};
+
 renderMaze();
 
 regenerateMaze.addEventListener("click", renderMaze);
 pathSolver.addEventListener("click", pathSolverFn);
+pathClearer.addEventListener("click", pathClearerFn);
